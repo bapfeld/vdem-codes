37456ebf474de8e39602273663381b536ff0dfd0
Brendan Apfeld <brendan.apfeld@utexas.edu>
2017-04-28 11:10:40 -0500

    add correct function file

---
+++ vcode_function.R
@@ -0,0 +1,33 @@
+# script to assign country codes based on various possible country names
+
+
+vcode <- function(df, country_column = "Country", vcode_csv_path = "Code/Country Codes/vcode-names.csv"){
+  cnamefoo <- rio::import(vcode_csv_path)
+  master_country_list <- cnamefoo$V2
+  c_column <- grep(country_column, names(df))
+  if(length(c_column) == 0){
+    warning("Execution halted. The country_column you specified was not found in this dataframe.")
+    return(df)
+    }else if(length(c_column) > 1){
+      warning("Execution halted. More than one column found with the specified country_column.")
+      return(df)
+    }else{
+      df_country <- df[[c_column]]
+      df_country <- as.character(df_country)
+      # check that the countries are actually in our master list
+      country_check <- df_country %in% master_country_list
+      if(sum(country_check) == length(country_check)){
+        vc <- lapply(df_country, function(x) match(x, master_country_list))
+        vc <- unlist(vc)
+        df$country_id <- cnamefoo$V1[vc]
+        df <- as.data.frame(df)
+        return(df)
+      }else{
+        boo <- df_country[which(country_check == F)]
+        boo <- boo[!duplicated(boo)]
+        warning("Execution halted. The following countries were not found in the master country file: ")
+        warning(cat(boo, sep = ", "))
+        return(df)
+      }
+    }
+}
