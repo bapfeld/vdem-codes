5f35bb647abf5b82ba4af4bafb90ea9b7e7c7ab4
Brendan Apfeld <brendan.apfeld@utexas.edu>
2018-01-02 14:20:54 -0600

    fix function error checking

--- vcode_to_country.R
+++ vcode_to_country.R
@@ -14,14 +14,14 @@ country_from_vcode <- function(df, country_id_column = "country_id", vnames_path
       stop("Execution halted. country_id_column cannot be converted to numeric.")
     }else{
       df[[id_column]] <- as.numeric(df[[id_column]])
-      if(sum(is.na(df[[id_column]]) > 0 | sum(df[[id_column]] == "") > 0)){
+      if((sum(is.na(df[[id_column]])) > 0) | (sum(df[[id_column]] == "") > 0)){
         stop("Execution halted. NA or blank country names detected.")
       }
       id_check <- df[[id_column]] %in% vnames$V1
-      if(sum(id_check) == length(country_check)){
-        df$vdem_country_name <- vnames$V2[match(df[[id_column]], vnames$V1)]
-        # is it possible to take a variable in the function call to determine what to call the new variable?
-        return(df)
+    if(sum(id_check) == length(id_check)){
+      df$vdem_country_name <- vnames$V2[match(df[[id_column]], vnames$V1)]
+      # is it possible to take a variable in the function call to determine what to call the new variable?
+      return(df)
       }else{
         bad_codes <- df[[id_column]][which(id_check == F)]
         bad_codes <- bad_codes[!duplicated(bad_codes)]
