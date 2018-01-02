06d6f683a3abfae102b8570438c30db5570faaf9
Brendan Apfeld <brendan.apfeld@utexas.edu>
2018-01-02 14:04:26 -0600

    draft scripts to keep vnames.csv up to date with changes to vdem-names.csv

--- Makefile
+++ Makefile
@@ -1,9 +1,11 @@
-.PHONY: all clean_csv clean
+.PHONY: all clean_csv clean names_csv_update
 
 stata_do: vcode_in_stata.R convert_csv_to_stata.R vcode-names.csv
 	R CMD BATCH convert_csv_to_stata.R
 clean_csv: sort_csv.R vcode-names.csv
 	R CMD BATCH sort_csv.R
+names_csv_update: update_vnames_csv.R vcode-names.csv
+	R CMD BATCH update_vnames_csv.R
 
 clean:
 	rm *.Rout
---
+++ update_vnames_csv.R
@@ -0,0 +1,22 @@
+# script to automatically add new country-codes to the vnames file
+
+library(dplyr)
+vcodes_master <- read.csv("vdem-names.csv", header = F, stringsAsFactors = F)
+vnames <- read.csv("vnames.csv", header = F, stringsAsFactors = F)
+
+vc <- as.numeric(levels(as.factor(vcodes_master$V1)))
+vn <- as.numeric(levels(as.factor(vnames$V1)))
+
+new_vc <- vc[which(! vc %in% vn)]
+
+v <- vcodes_master %>%
+  filter(V1 %in% new_vc) %>%
+  group_by(V1) %>%
+  do(slice(., 1)) %>%
+  data.frame
+
+vnames <- rbind(vnames, v) %>%
+  arrange(V1) %>%
+  data.frame
+
+write.table(vnames, file = "vnames.csv", row.names = F, col.names = F)
